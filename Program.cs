206b3d4c0c2f043751f9d6e420ce62664774fdf4
David Underhill <underhilld2@hotmail.com>
2017-06-07 22:22:32 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using AForge.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using TimeLapse.VidoeLogic;
+using NReco.VideoConverter;
+
+namespace TimeLapse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLineArgs.I.parseArgs(args, "height=640;width=480;framerate=5;numDays=2");
+            //var tempPath = CommandLineArgs.I.argAsString("tempPath");
+            var tempPath = "c:\\temp";
+            var sourcePath = CommandLineArgs.I.argAsString("sourcePath");
+            var imageType = CommandLineArgs.I.argAsString("imageType");
+            var outPath = CommandLineArgs.I.argAsString("outputPath");
+            var outputFile = CommandLineArgs.I.argAsString("outputFile");
+            int height = (int)CommandLineArgs.I.argAsLong("height");
+            int width = (int)CommandLineArgs.I.argAsLong("width");
+            int framerate = (int)CommandLineArgs.I.argAsLong("framerate");
+            int numDays = (int)CommandLineArgs.I.argAsLong("numDays");
+
+            //Console.WriteLine("Arg tempPath  : '{0}' ", tempPath);
+            Console.WriteLine("Arg SourcePath  : '{0}' ", sourcePath);
+            Console.WriteLine("Arg imageType  : '{0}' ", imageType);
+            Console.WriteLine("Arg OutPath  : '{0}' ", outPath);
+            Console.WriteLine("Arg outputFile  : '{0}' ", outputFile);
+            Console.WriteLine("Arg height  : '{0}' ", height);
+            Console.WriteLine("Arg width  : '{0}' ", width);
+            Console.WriteLine("Arg  framerate  : '{0}' ", framerate);
+            Console.WriteLine("Arg  numDays  : '{0}' ", numDays);
+
+            if (!string.IsNullOrEmpty(sourcePath) && !string.IsNullOrEmpty(imageType) && !string.IsNullOrEmpty(outPath))
+            {
+                var startTime = DateTime.UtcNow;
+                Console.WriteLine("Start processing: {0}", startTime.ToShortTimeString());
+                TimelapseProcessing.CreateTimelapse(sourcePath, imageType, tempPath, outputFile, width, height, framerate, numDays);
+                File.Copy(Path.Combine(tempPath, $"{outputFile}.mp4"), Path.Combine(outPath, $"{outputFile}.mp4"), true);
+                File.Copy(Path.Combine(tempPath, $"{outputFile}.webm"), Path.Combine(outPath, $"{outputFile}.webm"), true);
+                File.Delete(Path.Combine(tempPath, $"{outputFile}.mp4"));
+                File.Delete(Path.Combine(tempPath, $"{outputFile}.webm"));
+                var endTime = DateTime.UtcNow;
+                var duration = endTime - startTime;
+                Console.WriteLine("End Time: {0}", endTime.ToShortTimeString());
+                Console.WriteLine("End processing: {0}", duration.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Invalid parameters");
+            }
+
+         }
+
+    }
+
+    class CommandLineArgs
+    {
+        public static CommandLineArgs I
+        {
+            get
+            {
+                return m_instance;
+            }
+        }
+
+        public string argAsString(string argName)
+        {
+            if (m_args.ContainsKey(argName))
+            {
+                return m_args[argName];
+            }
+            else return "";
+        }
+
+        public long argAsLong(string argName)
+        {
+            if (m_args.ContainsKey(argName))
+            {
+                return Convert.ToInt64(m_args[argName]);
+            }
+            else return 0;
+        }
+
+        public double argAsDouble(string argName)
+        {
+            if (m_args.ContainsKey(argName))
+            {
+                return Convert.ToDouble(m_args[argName]);
+            }
+            else return 0;
+        }
+
+        public void parseArgs(string[] args, string defaultArgs)
+        {
+            m_args = new Dictionary<string, string>();
+            parseDefaults(defaultArgs);
+
+            foreach (string arg in args)
+            {
+                string[] words = arg.Split('=');
+                m_args[words[0]] = words[1];
+            }
+        }
+
+        private void parseDefaults(string defaultArgs)
+        {
+            if (defaultArgs == "") return;
+            string[] args = defaultArgs.Split(';');
+
+            foreach (string arg in args)
+            {
+                string[] words = arg.Split('=');
+                m_args[words[0]] = words[1];
+            }
+        }
+
+        private Dictionary<string, string> m_args = null;
+        static readonly CommandLineArgs m_instance = new CommandLineArgs();
+    }
+}
---
+++ VidoeLogic/TimelapseProcessing.cs
@@ -0,0 +1,39 @@
+﻿using AForge.Video.FFMPEG;
+using NReco.VideoConverter;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace TimeLapse.VidoeLogic
+{
+    public class TimelapseProcessing
+    {
+
+        public static void CreateTimelapse(string sourcePath, string fileExtension, string outputPath, string outputFile, int width, int height, int framePerSec, int numberOfDays)
+        {
+            string outputMp4 = $"{outputPath}/{outputFile}.mp4";
+            string outputWebm = $"{outputPath}/{outputFile}.webm";
+            using (var videoWriter = new VideoFileWriter())
+            {
+                videoWriter.Open(outputMp4, width, height, framePerSec, VideoCodec.MPEG4);
+
+                DirectoryInfo info = new DirectoryInfo(sourcePath);
+                var fileEntries = info.GetFiles(fileExtension, SearchOption.AllDirectories).Where(rec => rec.CreationTime > System.DateTime.Now.AddDays(System.Math.Abs(numberOfDays) * -1)).OrderBy(p => p.CreationTime).ToArray();
+
+                foreach (var fileName in fileEntries)
+                {
+                    using (Bitmap image = Bitmap.FromFile(fileName.FullName) as Bitmap)
+                    {
+                        videoWriter.WriteVideoFrame(image);
+                    }
+                }
+                videoWriter.Close();
+
+                var ffMpeg = new FFMpegConverter();
+                var settings = new ConvertSettings();
+                settings.VideoFrameRate = framePerSec;
+                ffMpeg.ConvertMedia(outputMp4, Format.mp4, outputWebm, Format.webm, settings);
+            }
+        }
+    }
+}
